5a813f2ea23b306f2222395deb85a874344c42af
handsom-coder <1186977762@qq.com>
2024-09-16 00:58:18 +0800

    写了intro mask,加了几个小文件。

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ max_tgt_word_nums = 8
 # embedding 的维度
 model_dim = 8
 
-torch_test = False
-if torch_test:
+is_str_len_rand = False
+if is_str_len_rand:
     src_len = torch.randint(2, 5, (batch_size,))
     tgt_len = torch.randint(2, 5, (batch_size,))
 else:
@@ -27,7 +27,7 @@ max_src_len = 5
 max_tgt_len = 5
 max_position_len = 5
 
-# 单词索引构成的句子,构建了batch,并且作了padding(默认值为0)
+# Step1: 单词索引构成的句子,构建了batch,并且作了padding(默认值为0)
 src_seq = torch.cat(
     [torch.unsqueeze(F.pad(torch.randint(1, max_src_word_nums, (L,)), (0, max_src_len - L)), 0) \
      for L in src_len])
@@ -35,13 +35,13 @@ tgt_seq = torch.cat(
     [torch.unsqueeze(F.pad(torch.randint(1, max_tgt_word_nums, (L,)), (0, max_tgt_len - L)), 0) \
      for L in tgt_len])
 
-# 构造word embedding
+# Step2: 构造word embedding
 src_embedding_table = nn.Embedding(max_src_word_nums + 1, model_dim)
 tgt_embedding_table = nn.Embedding(max_tgt_word_nums + 1, model_dim)
 src_word_embedding = src_embedding_table(src_seq)
 tgt_word_embedding = tgt_embedding_table(tgt_seq)
 
-# 构造position embedding:
+# Step3: 构造position embedding:
 # 列矩阵pos
 pos_mat = torch.arange(max_position_len).reshape(-1, 1)
 # 行矩阵i
@@ -62,7 +62,7 @@ tgt_pos = torch.cat([torch.unsqueeze(torch.arange(max_tgt_len), 0) for _ in tgt_
 src_pos_embedding = pos_embedding(src_pos)
 tgt_pos_embedding = pos_embedding(tgt_pos)
 
-# 构造encoder的self-attention mask
+# Step4: 构造encoder的self-attention mask
 # mask 的shape:[batch_size, max_src_len, max_src_len],值为1或-inf
 valid_encoder_pos = torch.unsqueeze(
     torch.cat([torch.unsqueeze(F.pad(torch.ones(L), (0, max_src_len - L)), 0) for L in src_len]), 2)
@@ -70,10 +70,30 @@ valid_encoder_pos_matrix = torch.bmm(valid_encoder_pos, valid_encoder_pos.transp
 invalid_encoder_pos_matrix = 1 - valid_encoder_pos_matrix
 mask_self_attention = invalid_encoder_pos_matrix.to(torch.bool)
 
-# 按理来说这里的score应当是与之前得到的word embedding 或者position embedding有关的一个矩阵。
-# 目前还不清楚为何要使用随机生成的这个score。
-score = torch.randn(batch_size, max_src_len, max_src_len)
-
+# 目前并不清楚为何要使用随机生成的这个score。
+is_score_set = True
+if is_score_set:
+    score = torch.randn(batch_size, max_src_len, max_src_len)
+else:
+    # 这里应该是根据src_word_embedding和src_pos_embedding通过某种计算得到的score(这个想法对吗？)
+    # 这里尝试给出了score的表达式。矩阵的shape是没错的，但是并不确定是否正确。
+    score = torch.bmm(src_word_embedding, src_pos_embedding.transpose(1, 2))
 masked_score = score.masked_fill(mask=mask_self_attention, value=-1e9)
 prob = F.softmax(masked_score, dim=-1)
-print(score, "\n", prob)
+# print("{0}\n{1}".format(score, prob))
+
+# Step5: 构造intro_attention的mask
+# Q @ K^T shape: [batch_size, max_tgt_len, max_src_len]
+
+# just like valid_encoder_pos
+valid_decoder_pos = torch.unsqueeze(
+    torch.cat([torch.unsqueeze(F.pad(torch.ones(L), (0, max_tgt_len - L)), 0) for L in tgt_len]), 2)
+valid_cross_pos_matrix = torch.bmm(valid_decoder_pos, valid_encoder_pos.transpose(1, 2))
+invalid_cross_pos_matrix = 1 - valid_cross_pos_matrix
+mask_cross_attention = invalid_cross_pos_matrix.to(torch.bool)
+
+# 在这之后要做的事情和前面差不多,mask_fill一下用于完成，代码就不写了
+
+# Step6: 构造decoder self-attention的mask
+valid_tri_matrix = [F.pad(torch.tril(torch.ones(L, L)), (0, (max_tgt_len - L), 0, (max_tgt_len - L))) for L in tgt_len]
+print("{0}".format(valid_tri_matrix))
