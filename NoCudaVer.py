ce45ce3e23419e2c18a47b8734757cfc0d67182e
handsom-coder <1186977762@qq.com>
2024-09-22 10:28:10 +0800

    在notebook里面绘制了ReLU和Tanh的区别

--- NoCudaVer.py
+++ NoCudaVer.py
@@ -94,9 +94,9 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        '''
+        """
         x: [seq_len, batch_size, d_model]
-        '''
+        """
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
@@ -222,12 +222,12 @@ class DecoderLayer(nn.Module):
         self.pos_ffn = PoswiseFeedForwardNet()
 
     def forward(self, dec_inputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask):
-        '''
+        """
         dec_inputs: [batch_size, tgt_len, d_model]
         enc_outputs: [batch_size, src_len, d_model]
         dec_self_attn_mask: [batch_size, tgt_len, tgt_len]
         dec_enc_attn_mask: [batch_size, tgt_len, src_len]
-        '''
+        """
         # dec_outputs: [batch_size, tgt_len, d_model], dec_self_attn: [batch_size, n_heads, tgt_len, tgt_len]
         dec_outputs, dec_self_attn = self.dec_self_attn(dec_inputs, dec_inputs, dec_inputs, dec_self_attn_mask)
         # dec_outputs: [batch_size, tgt_len, d_model], dec_enc_attn: [batch_size, h_heads, tgt_len, src_len]
--- TODO.md
+++ TODO.md
@@ -4,13 +4,13 @@
 - [ ] ```dataloader```的参数的含义
 - [ ] ```LongTensor```的作用
 - [ ] ```position_encoding```的```forward```
-- [ ] 根据这里的代码中的dec_inputs和dec_ouputs,是否就是资料中所提到的teacher forcing?
+- [ ] 根据这里的代码中的dec_inputs和dec_outputs,是否就是资料中所提到的teacher forcing?
 - [ ] ```model(enc_inputs, dec_inputs)```为什么可以不调用```forward```?
 - [ ] 在这个代码示例里面，word embedding是不包含语义信息的吗？
 
 # 运行
 
-- [ ] 多次运行该代码会改变结果吗？
+- [ ] 多次运行该代码会改变结果吗？(输出的随机性)
 - [ ] ```register_buffer()```
 - [ ] 查看```make_data()```的返回值的形状
 - [ ] 尝试修改代码，计算训练时的perplexity
