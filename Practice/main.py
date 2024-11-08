2f5dc72acf2ef547feaa44b21d95061ff6b52a15
handsome-coder <1186977762@qq.com>
2024-11-08 13:31:35 +0800

    简单的模型（gpt2）使用的示例

--- Practice/main.py
+++ Practice/main.py
@@ -1,21 +1,14 @@
-import os
-from datasets import concatenate_datasets, load_dataset
-
-
-def dataset_to_text(dataset, output_filename="data.txt"):
-	"""Utility function to save dataset text to disk,
-	useful for using the texts to train the tokenizer
-	(as the tokenizer accepts files)"""
-	with open(output_filename, "w") as f:
-		for t in dataset["text"]:
-			print(t, file=f)
-
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 if __name__ == "__main__":
-	bookcorpus = load_dataset("bookcorpus", split="train", encoding='utf-8')
-	wiki = load_dataset("wikipedia", "20230601.en", split="train")
-	wiki = wiki.remove_columns([col for col in wiki.column_names if col != "text"])
-	dataset = concatenate_datasets([bookcorpus, wiki])
-	d = dataset.train_test_split(test_size=0.1)
-	dataset_to_text(d["train"], "train.txt")
-	dataset_to_text(d["test"], "test.txt")
+	model_name = "gpt2"
+	tokenizer = AutoTokenizer.from_pretrained(model_name)
+	model = AutoModelForCausalLM.from_pretrained(model_name)
+	
+	input_text = "Once upon a time,there was a kid"
+	
+	inputs = tokenizer(input_text, return_tensors="pt")
+	outputs = model.generate(**inputs, max_length=80, do_sample=True, top_p=0.95, temperature=0.7)
+	generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+	
+	print(generated_text)
--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # 说明
 
-- 目前还没有多少代码的修改，暂时不做说明。
+对于transformers等库相关的代码，可以在对应目录使用以下命令一键启动：
 
-# 论文（前置信息）阅读
+```
+export HF_ENDPOINT=https://hf-mirror.com && python main.py
+```
+
+### 准备测试的几个模型：
+
+```
+Langboat/bloom-1b4-zh //效果一般
+IDEA-CCNL/Wenzhong2.0-GPT2-3.5B-chinese
+```
+
+### 论文（前置信息）阅读
 
 - [ ] BPE和word-piece的区别。
 - [ ] GPT-2的full scoring和partial scoring指的是什么。
 - [ ] 各种优化器的发展历程和比较。
 
-# 计划
+### 计划
 
 > - 《大规模语言模型：从理论到实践》重点章节：**2、5、6、7**。每个章节，概念过完以后要跟着做代码实践。（11.17前完成）
 > - GPT2:
